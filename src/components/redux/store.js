36dc1a925059f594334cd1184ce4114b9043cf28
kwin64 <kwin649011@gmail.com>
2023-07-17 22:22:22 +0300

    feat: redux-presist (localStorage)

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,10 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import todolistReducer from './slices/todolistSlice'
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const store = configureStore({
-	reducer: {
-		todolist: todolistReducer
-	}
+const rootReducer = combineReducers({
+	todolist: todolistReducer
 })
 
+const persistConfig = {
+	key: 'root',
+	storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+			}
+		})
+})
+export const persistor = persistStore(store)
 export default store
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App'
-import store from './components/redux/store'
+import store, { persistor } from './components/redux/store'
 import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<Provider store={store}>
-		<App />
+		<PersistGate
+			loading={null}
+			persistor={persistor}>
+			<App />
+		</PersistGate>
 	</Provider>
 )
